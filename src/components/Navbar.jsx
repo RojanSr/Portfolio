d54258ba88f4c86ef08d877d5c80885576155f98
RojanSr <rairojan1116@gmail.com>
2023-05-29 22:29:30 +0545

    create responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
 export default function Navbar(props) {
+  // const hamDiv = document.getElementById("hamburger-menu");
+
+  const [hamDiv, setHamDiv] = React.useState(false);
+
+  const hamStyle = hamDiv
+    ? {
+        display: "block",
+      }
+    : {
+        display: "none",
+      };
+
+  function handleHam() {
+    setHamDiv(!hamDiv);
+  }
+
   return (
     <nav>
       <h1 className="nav--header">
@@ -23,7 +39,28 @@ export default function Navbar(props) {
         <li className="nav--item">
           <a href="#contact-section">Contact</a>
         </li>
+        <div className="toggleHamburger" onClick={handleHam}>
+          <i className="fa-solid fa-bars"></i>
+        </div>
       </ul>
+
+      <div className="hamburger-div" id="hamburger-menu" style={hamStyle}>
+        <i className="fa-solid fa-xmark" onClick={handleHam}></i>
+        <ul>
+          <li className="hamnav--item">
+            <a href="#home-section">Home</a>
+          </li>
+          <li className="hamnav--item">
+            <a href="#skills-section">Skills</a>
+          </li>
+          <li className="hamnav--item">
+            <a href="#project-section">Projects</a>
+          </li>
+          <li className="hamnav--item">
+            <a href="#contact-section">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
